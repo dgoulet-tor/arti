dd17ba05d0a76e2a4e03d8c7b0aabd8d1beb5915
Nick Mathewson <nickm@torproject.org>
2020-05-08 00:05:49 -0400

    llcrypto: small documentation improvements

--- tor-llcrypto/src/lib.rs
+++ tor-llcrypto/src/lib.rs
@@ -1,14 +1,18 @@
-//! This crate wraps other crates that implement lower-level
-//! cryptographic functionality that Tor does not implement itself.
-//! In some cases the functionality is just re-exported; in others, it
-//! is wrapped to present a conseistent interface.
+//! Low-level crypto implementations for Tor.
+//!
+//! This crate doesn't have anything interesting: it just wraps other
+//! crates that implement lower-level cryptographic functionality that
+//! Tor does not implement itself.  In some cases the functionality is
+//! just re-exported; in others, it is wrapped to present a
+//! conseistent interface.
 //!
 //! Encryption is implemented in `cipher`, digests are in `d`, and
 //! public key cryptography (including signatures, encryption, and key
 //! agreement) are in `pk`.
 //!
-//! When possible, everything here should be accessed via traits from
-//! the rust-crypto project, as re-exported from the traits module.
+//! When possible, everything here should be accessed via traits.
+//! We're using traits from the rust-crypto project, as
+//! re-exported from the traits module.
 
 // TODO -- the long-term intention here is that this functionality
 // should be replaceable at compile time with other implementations.
