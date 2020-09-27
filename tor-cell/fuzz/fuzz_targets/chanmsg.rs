2feb750ba1746d4c8cd990335e347dc1a3c82351
Nick Mathewson <nickm@torproject.org>
2020-09-27 03:16:53 -0400

    fix fuzzing targets that broke from recent refactoring

--- tor-cell/fuzz/fuzz_targets/chanmsg.rs
+++ tor-cell/fuzz/fuzz_targets/chanmsg.rs
@@ -1,9 +1,8 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use tor_proto::chancell::codec::ChannelCodec;
-use futures_codec::Decoder;
+use tor_cell::chancell::codec::ChannelCodec;
 
 fuzz_target!(|data: &[u8]| {
     let mut bytes = data.into();
-    let _ = ChannelCodec::new(4).decode(&mut bytes);
+    let _ = ChannelCodec::new(4).decode_cell(&mut bytes);
 });
