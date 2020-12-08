5fc116ad7a4ee7e5e0200b6a39a3196b9e477568
Nick Mathewson <nickm@torproject.org>
2020-12-07 21:22:12 -0500

    Tolerate attempt to write a non-UTF-8 HTTP header.
    previously, we would panic.

--- tor-dirclient/src/util.rs
+++ tor-dirclient/src/util.rs
@@ -5,8 +5,12 @@ pub(crate) fn encode_request(req: http::Request<()>) -> String {
     let mut s = format!("{} {} HTTP/1.0\r\n", req.method(), req.uri());
 
     for (key, val) in req.headers().iter() {
-        // XXXX-A1 Unwrap is a bit naughty, but should be harmless
-        s.push_str(&format!("{}: {}\r\n", key, val.to_str().unwrap()));
+        s.push_str(&format!(
+            "{}: {}\r\n",
+            key,
+            val.to_str()
+                .expect("Added an HTTP header that wasn't UTF-8!")
+        ));
     }
     s.push_str("\r\n");
     s
