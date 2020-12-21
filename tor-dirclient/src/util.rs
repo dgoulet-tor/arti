87cb44a4e5bfec8ee85e1a5b1fecfcc42d40b358
Nick Mathewson <nickm@torproject.org>
2020-12-21 11:57:10 -0500

    Basic test for tor-dirclient::util

--- tor-dirclient/src/util.rs
+++ tor-dirclient/src/util.rs
@@ -15,3 +15,28 @@ pub(crate) fn encode_request(req: http::Request<()>) -> String {
     s.push_str("\r\n");
     s
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn fromat() {
+        let req = http::Request::builder()
+            .method("GET")
+            .uri("/index.html")
+            .body(())
+            .unwrap();
+        assert_eq!(encode_request(req), "GET /index.html HTTP/1.0\r\n\r\n");
+        let req = http::Request::builder()
+            .method("GET")
+            .uri("/index.html")
+            .header("X-Marsupial", "Opossum")
+            .body(())
+            .unwrap();
+        assert_eq!(
+            encode_request(req),
+            "GET /index.html HTTP/1.0\r\nx-marsupial: Opossum\r\n\r\n"
+        );
+    }
+}
