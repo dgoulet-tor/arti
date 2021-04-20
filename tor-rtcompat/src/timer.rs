3fc66306c5df0fc21fc12d07ceea08123f785926
Nick Mathewson <nickm@torproject.org>
2021-04-20 09:34:34 -0400

    Tests for sleep_until_wallclock.

--- tor-rtcompat/src/timer.rs
+++ tor-rtcompat/src/timer.rs
@@ -163,18 +163,19 @@ where
     }
 }
 
+/// We never sleep more than this much, in case our system clock jumps.
+///
+/// Note that there's a tradeoff here: Making this duration
+/// shorter helps our accuracy, but makes us wake up more
+/// frequently and consume more CPU.
+const MAX_SLEEP: Duration = Duration::from_secs(600);
+
 /// Return the amount of time we should wait next, when running
 /// sleep_until_wallclock().  Also return a boolean indicating whether we
 /// expect this to be the final delay.
 ///
 /// (This is a separate function for testing.)
 fn calc_next_delay(now: SystemTime, when: SystemTime) -> (bool, Duration) {
-    /// We never sleep more than this much, in case our system clock jumps.
-    ///
-    /// Note that there's a tradeoff here: Making this duration
-    /// shorter helps our accuracy, but makes us wake up more
-    /// frequently and consume more CPU.
-    const MAX_SLEEP: Duration = Duration::from_secs(600);
     let remainder = when
         .duration_since(now)
         .unwrap_or_else(|_| Duration::from_secs(0));
@@ -188,6 +189,12 @@ fn calc_next_delay(now: SystemTime, when: SystemTime) -> (bool, Duration) {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::mock::time::MockSleepProvider;
+    use crate::test_with_runtime;
+
+    use futures::channel::oneshot;
+    use std::sync::atomic::{AtomicBool, Ordering};
+
     #[test]
     fn sleep_delay() {
         fn calc(now: SystemTime, when: SystemTime) -> Duration {
@@ -205,5 +212,167 @@ mod test {
         assert_eq!(calc(target - second, target), second);
         assert_eq!(calc(target - minute * 9, target), minute * 9);
         assert_eq!(calc(target - minute * 11, target), minute * 10);
+    }
+
+    #[test]
+    fn timeouts() {
+        fn setup() -> (
+            MockSleepProvider,
+            oneshot::Sender<()>,
+            Timeout<oneshot::Receiver<()>, crate::mock::time::Sleeping>,
+        ) {
+            let start = SystemTime::now();
+            let (send, recv) = oneshot::channel::<()>();
+            let mock_sp = MockSleepProvider::new(start);
+            let ten_min = Duration::new(10 * 60, 0);
+            let timeout_future = mock_sp.timeout(ten_min, recv);
+            (mock_sp, send, timeout_future)
+        }
+
+        // The timeout occurs.
+        test_with_runtime(|_| async {
+            let (mock_sp, _send, timeout_future) = setup();
+            mock_sp.advance(Duration::new(3600, 0));
+            assert_eq!(timeout_future.await, Err(TimeoutError));
+        });
+        // The data is ready immediately.
+        test_with_runtime(|_| async {
+            let (_, send, timeout_future) = setup();
+            send.send(()).unwrap();
+            assert_eq!(timeout_future.await, Ok(Ok(())));
+        });
+        // The data is ready after a little while
+        test_with_runtime(|_| async {
+            let (mock_sp, send, timeout_future) = setup();
+            mock_sp.advance(Duration::new(10, 0));
+            send.send(()).unwrap();
+            assert_eq!(timeout_future.await, Ok(Ok(())));
+        });
+        // The data is ready _and_ the timout occurs.
+        test_with_runtime(|_| async {
+            let (mock_sp, send, timeout_future) = setup();
+            send.send(()).unwrap();
+            mock_sp.advance(Duration::new(3600, 0));
+            assert_eq!(timeout_future.await, Ok(Ok(())));
+        });
+        // Make sure that nothing happens too early.
+        test_with_runtime(|_| async {
+            let (mock_sp, _send, timeout_future) = setup();
+            mock_sp.advance(Duration::new(300, 0));
+            assert_eq!(timeout_future.now_or_never(), None);
+        });
+    }
+
+    fn start() -> SystemTime {
+        // Yes this is a homestuck reference
+        SystemTime::UNIX_EPOCH + Duration::from_secs(1239657180)
+    }
+    const ONE_DAY: Duration = Duration::from_secs(86400);
+
+    #[test]
+    fn wallclock_simple() {
+        // Simple case: time goes by.
+        test_with_runtime(|_| async {
+            let mock_sp = MockSleepProvider::new(start());
+            let b = AtomicBool::new(false);
+            futures::join!(
+                async {
+                    mock_sp.sleep_until_wallclock(start() + ONE_DAY).await;
+                    b.store(true, Ordering::SeqCst);
+                },
+                async {
+                    while mock_sp.wallclock() < start() + ONE_DAY {
+                        assert_eq!(false, b.load(Ordering::SeqCst));
+                        mock_sp.advance(Duration::new(413, 0));
+                        // XXXX maybe I should yield here?
+                    }
+                }
+            );
+            assert_eq!(true, b.load(Ordering::SeqCst));
+        });
+    }
+
+    #[test]
+    fn wallclock_early() {
+        // Simple case 2: time goes by, but not enough of it.
+        test_with_runtime(|_| async {
+            let mock_sp = MockSleepProvider::new(start());
+            let b = AtomicBool::new(false);
+            let (send, mut recv) = oneshot::channel();
+            futures::join!(
+                async {
+                    let mut sleep = mock_sp.sleep_until_wallclock(start() + ONE_DAY).fuse();
+                    futures::select! {
+                        _ = sleep => b.store(true, Ordering::SeqCst),
+                        _ = recv => (),
+                    };
+                },
+                async {
+                    while mock_sp.wallclock() < start() + (ONE_DAY / 2) {
+                        assert_eq!(false, b.load(Ordering::SeqCst));
+                        mock_sp.advance(Duration::new(413, 0));
+                    }
+                    send.send(()).unwrap();
+                }
+            );
+            assert_eq!(false, b.load(Ordering::SeqCst));
+        });
+    }
+
+    #[test]
+    fn wallclock_jump_forward() {
+        // Clock jumps forward, so event triggers.
+        test_with_runtime(|_| async {
+            let mock_sp = MockSleepProvider::new(start());
+            let b = AtomicBool::new(false);
+            let i1 = mock_sp.now();
+            futures::join!(
+                async {
+                    mock_sp.sleep_until_wallclock(start() + ONE_DAY).await;
+                    b.store(true, Ordering::SeqCst);
+                },
+                async {
+                    mock_sp.jump_to(start() + ONE_DAY);
+                    mock_sp.advance(MAX_SLEEP); // have to rest some.
+                }
+            );
+            assert_eq!(true, b.load(Ordering::SeqCst));
+            let i2 = mock_sp.now();
+            assert!(i2 - i1 < ONE_DAY);
+        });
+    }
+
+    #[test]
+    fn wallclock_jump_backwards() {
+        // Clock jumps backward, so event does not trigger early.
+        test_with_runtime(|_| async {
+            let mock_sp = MockSleepProvider::new(start());
+            let b = AtomicBool::new(false);
+            let (send, mut recv) = oneshot::channel();
+            let i1 = mock_sp.now();
+            futures::join!(
+                async {
+                    let mut sleep = mock_sp.sleep_until_wallclock(start() + ONE_DAY).fuse();
+                    futures::select! {
+                        _ = sleep => b.store(true, Ordering::SeqCst),
+                        _ = recv => (),
+                    };
+                },
+                async {
+                    mock_sp.jump_to(start() - ONE_DAY);
+                    let mut elapsed = Duration::new(0, 0);
+                    while elapsed < (3 * ONE_DAY) / 2 {
+                        assert_eq!(false, b.load(Ordering::SeqCst));
+                        mock_sp.advance(Duration::new(413, 0));
+                        elapsed += Duration::new(413, 0);
+                    }
+                    send.send(()).unwrap();
+                }
+            );
+            assert_eq!(false, b.load(Ordering::SeqCst));
+            let i2 = mock_sp.now();
+            assert!(i2 - i1 > ONE_DAY);
+            assert!(mock_sp.wallclock() < start() + ONE_DAY);
+        });
     }
 }
