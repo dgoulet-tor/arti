dbb6654e83fe1eb00020714d8995505b06221d99
Nick Mathewson <nickm@torproject.org>
2021-05-03 09:23:28 -0400

    Add warnings to tor-rtmock

--- tor-rtmock/src/lib.rs
+++ tor-rtmock/src/lib.rs
@@ -5,6 +5,17 @@
 //! Currently, we support mocking the passage of time, making fake
 //! stream pairs, and impersonating the Internet.
 
+#![deny(missing_docs)]
+#![deny(clippy::await_holding_lock)]
+#![warn(clippy::clone_on_ref_ptr)]
+#![warn(clippy::cognitive_complexity)]
+#![deny(clippy::debug_assert_with_mut_call)]
+#![deny(clippy::exhaustive_enums)]
+#![deny(clippy::exhaustive_structs)]
+#![deny(clippy::expl_impl_clone_on_copy)]
+#![deny(clippy::missing_docs_in_private_items)]
+#![warn(clippy::rc_buffer)]
+
 pub mod io;
 pub mod net;
 pub mod time;
