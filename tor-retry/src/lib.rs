bcb26da1dfacaed5ec8798e2ab820f443dcdfd4c
Nick Mathewson <nickm@torproject.org>
2021-05-24 08:38:18 -0400

    Improve crate documentation for tor-retry and tor-socksproto

--- tor-retry/src/lib.rs
+++ tor-retry/src/lib.rs
@@ -1,7 +1,46 @@
 //! Helpers to implement retry-related functionality.
 //!
 //! Right now, this crate only has an error type that we use when we
-//! retry something a few times, and they all fail.
+//! retry something a few times, and they all fail.  Instead of
+//! returning only a single error, it records _all of the errors
+//! received, in case they are different.
+//!
+//! This crate is part of
+//! [Arti](https://gitlab.torproject.org/tpo/core/arti/), a project to
+//! implement Tor in Rust.  It's used by higher-level crates that retry
+//! operations.
+//!
+//! ## Design notes
+//!
+//! XXXX We'll want to refactor this one.  It might be better in a
+//! crate called retry-error or something, since it isn't
+//! tor-specific.
+//!
+//! The [`RetryError`] type might be more generally useful in the
+//! future, if it gets a stable interface, and if we can make it stop
+//! depending on [`anyhow`].
+//!
+//! Maybe this error type should be parameterized on an input error type.
+//!
+//! # Example
+//!
+//! ```
+//! # fn some_operation() -> Result<(),anyhow::Error> {Ok(())}
+//! # fn demo() -> Result<(),tor_retry::RetryError> {
+//! use tor_retry::RetryError;
+//!
+//! const N_ATTEMPTS: usize = 10;
+//! let mut err = RetryError::while_doing("perform an example operation");
+//! for _ in 0..N_ATTEMPTS {
+//!     match some_operation() {
+//!         Ok(val) => return Ok(val),
+//!         Err(e) => err.push(e),
+//!     }
+//! }
+//! // All attempts failed; return all the errors.
+//! return Err(err)
+//! # }
+//! ```
 
 #![deny(missing_docs)]
 #![deny(unreachable_pub)]
--- tor-socksproto/src/lib.rs
+++ tor-socksproto/src/lib.rs
@@ -1,12 +1,23 @@
 //! Implements SOCKS in the flavors provided by Tor.
 //!
+//! # Overview
+//!
 //! SOCKS is an old and somewhat janky protocol for telling a TCP
 //! proxy where to connect.  Versions 4, 4a, and 5 are sometimes
 //! encountered in the wild.
 //!
-//! This crate tries to hide the actual details of the protocol, and
-//! expose a stateful handshake type that eventually provides a [SocksRequest]
-//! or an error.
+//! The `tor-socksproto` crate tries to hide the actual details of the
+//! protocol, and expose a stateful handshake type that eventually
+//! provides a [SocksRequest] or an error.  It is a part of
+//! [Arti](https://gitlab.torproject.org/tpo/core/arti/), a project to
+//! implement Tor in Rust.  At present, it is only used to provide a
+//! SOCKS proxy _over_ the Tor network, but eventually it may be used
+//! to implement support for connecting to the Tor network over a
+//! SOCKS proxy.
+//!
+//! This crate may be a good choice for you if you need a SOCKS
+//! implementation that "behaves like Tor", but otherwise it is
+//! probably better to use some other SOCKS crate.
 //!
 //! For more information about SOCKS:
 //!
@@ -20,6 +31,29 @@
 //!     [socks-extensions.txt](https://spec.torproject.org/socks-extensions)
 //!     for a description of Tor's extensions and restrictions on the
 //!     SOCKS protocol.
+//!
+//! ## Design notes
+//!
+//! Arti uses this crate instead of some other SOCKS implementation,
+//! for two reasons:
+//!
+//!  * First, because we need to support Tor SOCKS extensions.
+//!  * Second, and because we sometimes need to see particular details
+//!    of the individual handshakes that most other SOCKS
+//!    implementations don't expose.  (For example, if we are told to
+//!    connect to a raw IP address, the type of the handshake can help
+//!    us guess whether that IP address came from a DNS response–in
+//!    which case we should warn about a possible DNS leak.)
+//!
+//! Currently, `tor-socksproto` does no networking code: it _only_
+//! implements the server (proxy) side of the SOCKS handshake by
+//! handling a series of bytes.  We may (or may not) want to add
+//! network functionality to this crate or elsewhere in the future.
+//! We'll definitely want to add client functionality.
+//!
+//! Possibly, this approach will prove useful for other uses.  If it
+//! does, We can put the tor-only functionality behind a Cargo build
+//! feature, so that others can use this crate more safely.
 
 #![deny(missing_docs)]
 #![deny(unreachable_pub)]
