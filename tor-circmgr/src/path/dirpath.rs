4e3e1982be6193ca490f4dbeea6778e9f29dca5c
Nick Mathewson <nickm@torproject.org>
2021-06-16 13:24:11 -0400

    circmgr: tests for path/dirpath.rs

--- tor-circmgr/src/path/dirpath.rs
+++ tor-circmgr/src/path/dirpath.rs
@@ -44,3 +44,71 @@ impl DirPathBuilder {
         ))
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use tor_netdir::testnet;
+
+    #[test]
+    fn dirpath_relay() {
+        let netdir = testnet::construct_netdir();
+        let mut rng = rand::thread_rng();
+        let dirinfo = (&netdir).into();
+
+        for _ in 0..1000 {
+            let p = DirPathBuilder::default().pick_path(&mut rng, dirinfo);
+            let p = p.unwrap();
+            assert!(p.exit_relay().is_none());
+            assert_eq!(p.len(), 1);
+            if let crate::path::TorPathInner::OneHop(r) = p.inner {
+                assert!(r.is_dir_cache());
+            } else {
+                panic!("Generated the wrong kind of path.");
+            }
+        }
+    }
+
+    #[test]
+    fn dirpath_fallback() {
+        let fb = vec![
+            FallbackDir::builder()
+                .rsa_identity([0x01; 20].into())
+                .ed_identity([0x01; 32].into())
+                .orport("127.0.0.1:9000".parse().unwrap())
+                .build()
+                .unwrap(),
+            FallbackDir::builder()
+                .rsa_identity([0x03; 20].into())
+                .ed_identity([0x03; 32].into())
+                .orport("127.0.0.1:9003".parse().unwrap())
+                .build()
+                .unwrap(),
+        ];
+        let dirinfo = DirInfo::Fallbacks(&fb[..]);
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..10 {
+            let p = DirPathBuilder::default().pick_path(&mut rng, dirinfo);
+            let p = p.unwrap();
+            assert!(p.exit_relay().is_none());
+            assert_eq!(p.len(), 1);
+
+            if let crate::path::TorPathInner::FallbackOneHop(f) = p.inner {
+                assert!(std::ptr::eq(f, &fb[0]) || std::ptr::eq(f, &fb[1]));
+            } else {
+                panic!("Generated the wrong kind of path.");
+            }
+        }
+    }
+
+    #[test]
+    fn dirpath_no_fallbacks() {
+        let fb = vec![];
+        let dirinfo = DirInfo::Fallbacks(&fb[..]);
+        let mut rng = rand::thread_rng();
+
+        let err = DirPathBuilder::default().pick_path(&mut rng, dirinfo);
+        assert!(matches!(err, Err(Error::NoRelays(_))));
+    }
+}
