7c9373a32fe0673ac7b0163ca46d31e498799830
Nick Mathewson <nickm@torproject.org>
2021-08-04 15:59:36 -0400

    Add my "fuzz everything" script that I like to run overnight

---
+++ maint/fuzz_it_all.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+trap 'kill $(jobs -p)' EXIT
+
+for d in ./*/fuzz; do
+    pushd "$(dirname "$d")"
+    for fuzzer in $(cargo +nightly fuzz list); do
+	echo "$fuzzer"
+	cargo +nightly fuzz build "$fuzzer"
+    done
+    popd
+done
+
+
+#JOBS=4
+
+exit 0
+
+while true; do
+    for d in ./*/fuzz; do
+	pushd "$(dirname "$d")"
+	for fuzzer in $(cargo +nightly fuzz list); do
+	    set +e
+	    timeout 20m cargo +nightly fuzz run "$fuzzer"
+	    status="$?"
+	    set -e
+	    case "$status" in
+		0)
+  		    # Successful exit?
+		    ;;
+		124)
+		    # This is a timeout
+		    ;;
+		*)
+		    exit 1
+		    ;;
+	    esac
+	done
+	popd
+    done
+done
+
+
+# wait -n
+
